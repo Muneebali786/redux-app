952120170b87259c7aa75fef3d09edd2ad1791bd
Muneeb Ali <muneebrashid786@gmail.com>
2020-10-27 09:58:26 +0500

    First Commit

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import {createStore , applyMiddleware } from 'redux';
+import rootReducer from './reducers/index.js';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas/index';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = applyMiddleware(sagaMiddleware)(createStore)(rootReducer);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
